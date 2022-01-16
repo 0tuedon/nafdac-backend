4e0f174ff16fefb74f6bc75281d75604b5e4f4ed
0tuedon <tuedonmofe@gmail.com>
2022-01-16 06:39:03 +0100

    This should work now

--- index.js
+++ index.js
@@ -18,17 +18,19 @@ mongoose.connect(process.env.DATABASE).then(
 })
 
 app.set('view engine','ejs')
-app.use(bodyparser.json())
-app.use("/",apiRoute)
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 const corsOrigins = {
-    origin: ['http://localhost:3000', 'https://localhost:3000','https://nafdac.netlify.app']
+    origin: ['http://localhost:3000', 'https://localhost:3000', 'https://nafdac.netlify.app']
 }
 app.use(cors(corsOrigins))
+
+app.use(bodyparser.json())
+app.use("/",apiRoute)
 
 app.get("/",(req,res)=>{
     res.render("index")
