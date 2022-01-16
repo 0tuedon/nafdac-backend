d74a83fd4a86c595d5709603bc23aa7f1cb37563
0tuedon <tuedonmofe@gmail.com>
2022-01-16 06:30:28 +0100

    Chore: Restructure cors policy

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ mongoose.connect(process.env.DATABASE).then(
 app.set('view engine','ejs')
 app.use(bodyparser.json())
 app.use("/",apiRoute)
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const corsOrigins = {
     origin: ['http://localhost:3000', 'https://localhost:3000','https://nafdac.netlify.app']
 }
